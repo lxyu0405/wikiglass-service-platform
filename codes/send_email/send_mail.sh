876b971fb7752871929e706a77cf2c2e457445b9
oper <oper@ccmir.cite.hku.hk>
2017-01-31 10:41:52 +0800

    finish testing send_mail.sh & weekly_db_update.sh

---
+++ codes/send_email/send_mail.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sendMailDir="/home/oper/wikiglass-data-service/wikiglass-service-platform/codes/send_email/"
+
+now=$(date +"%T")
+
+echo "$now Start weekly_email.py"
+/usr/local/bin/python ${sendMailDir}send_mail.py
+now1=$(date +"%T")
+echo "$now1 Complete weekly_email.py"
+echo " "
+
+echo "$now1 Start sendEmail"
+for txt in /home/oper/wikiglass-data-service/resources/email-txt/*
+do
+	if [ -f "$txt" ];then
+		/usr/sbin/sendmail -t < $txt
+	fi
+done
+now2=$(date +"%T")
+echo "$now2 Complete sendEmail"
