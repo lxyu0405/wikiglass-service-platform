1c7b016a776eaa680f43d299442e4d33138e858b
oper <oper@ccmir.cite.hku.hk>
2017-01-31 10:38:35 +0800

    finish testing send_mail.sh weekly_db_update.sh

--- shell_scripts/send_mail.sh
+++ shell_scripts/send_mail.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-sendMailDir="/home/oper/wikiglass-data-service/wikiglass-service-platform/codes/send_email/"
-
-now=$(date +"%T")
-
-echo "$now Start weekly_email.py"
-/usr/local/bin/python ${sendMailDir}send_mail.py
-now1=$(date +"%T")
-echo "$now1 Complete weekly_email.py"
-echo " "
-
-
+../codes/send_email/send_mail.sh
--- shell_scripts/weekly_db_update.sh
+++ shell_scripts/weekly_db_update.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-# Weekly Database Update
-
-weeklyPyDir="/home/oper/wikiglass-data-service/codes/data_analysis_weekly/"
-
-now=$(date +"%T")
-
-# Weekly_revision_count, Weekly_word_amendment, Weekly_word_count, Weekly_sentence_lvl
-# Prepare data for timeline view in weekly basis
-echo "$now Start weekly_timeline.py"
-/usr/local/bin/python ${weeklyPyDir}weekly_timeline.py
-now1=$(date +"%T")
-echo "$now1 Complete weekly_timeline.py"
+../codes/data_analysis_weekly/weekly_db_update.sh
